e8a77279cdab2f572cc1ca1a198eaa2e0b233fc5
Rafica <rafica.ar@gmail.com>
2013-11-22 15:18:07 -0700

    Added facebook login

--- CookStory/src/com/cookstory/MainActivity.java
+++ CookStory/src/com/cookstory/MainActivity.java
@@ -1,8 +1,14 @@
 package com.cookstory;
-
+import com.facebook.*;
+import com.facebook.model.*;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.widget.TextView;
+import android.content.Intent;
+
+
+
 
 /**
  * The Main Activity.
@@ -19,7 +25,33 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// start Facebook Login
+		Session.openActiveSession(this, true, new Session.StatusCallback() {
 
+		    // callback when session changes state
+		    @Override
+		    public void call(Session session, SessionState state, Exception exception) {
+		    	if (session.isOpened()) {
+		    		
+		    		// make request to the /me API
+		    		Request.newMeRequest(session, new Request.GraphUserCallback() {
+
+		    			  // callback after Graph API response with user object
+		    			  @Override
+		    			  public void onCompleted(GraphUser user, Response response) {
+		    			    if (user != null) {
+		    			      TextView welcome = (TextView) findViewById(R.id.welcome);
+		    			      welcome.setText("Hello " + user.getName() + "!");
+		    			    }
+		    			  }
+		    			}).executeAsync();
+		    	}
+
+		    }
+		  });
+		  
+		
 		// Start up RegisterActivity right away
 		Intent intent = new Intent(this, RegisterActivity.class);
 		startActivity(intent);
@@ -27,4 +59,12 @@ public class MainActivity extends Activity {
 		// finish it after launching RegisterActivity
 		finish();
 	}
+	
+	//update the active session
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	  super.onActivityResult(requestCode, resultCode, data);
+	  Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+	}
+	
 }
